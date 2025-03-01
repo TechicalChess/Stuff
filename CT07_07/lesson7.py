dbfdd34a9f81a898d00e2775eb88dd235aec5af3
TechicalChess <chua_yun_jie@students.edu.sg>
2025-03-01 06:30:05 +0000

    Mar 1, 2025, 6:30 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,2 +1,11 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
+students = []
+
+student1 = ["Person", 12345678, "Running"]
+student2 = ["Personal", 23456789, "Walking"]
+student3 = ["Guy", 34567890, "swimming"]
+
+students.append(student1)
+for student in students:
+    
