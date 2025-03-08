e2c2a76051112694a29b60eddf788de32be49513
TechicalChess <chua_yun_jie@students.edu.sg>
2025-03-08 06:14:52 +0000

    Mar 8, 2025, 6:14 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,28 @@
-print("Hello from lesson 1")
+# print("Hello from lesson 1")
 
+# for i in range(21):
+#     print(i)
+
+# for i in range(1,31):
+#     print(i)
+
+# for i in range(2,25,2):
+#     print(i)
+
+# for i in range(99,6,-2):
+#     print(i)
+
+# for i in "hello":
+#     print(i)
+
+# counter = 2
+# while counter < 26:
+#     print(counter)
+#     counter += 2
+
+# counter = 1
+# while counter < 11:
+#     print(counter)
+#     if (counter == 5):
+#         break
+#     counter += 1
--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,11 +1,45 @@
-# print("Hello from lesson 7")
+# # print("Hello from lesson 7")
 
-students = []
+# students = []
 
-student1 = ["Person", 12345678, "Running"]
-student2 = ["Personal", 23456789, "Walking"]
-student3 = ["Guy", 34567890, "swimming"]
+# student1 = ["Person", 12345678, "Running"]
+# student2 = ["Personal", 23456789, "Walking"]
+# student3 = ["Guy", 34567890, "swimming"]
 
-students.append(student1)
-for student in students:
+# students.append(student1)
+# for student in students:
     
+
+# list1=[34,69,2]
+# list2=[98,99,3]
+
+# fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
+# index = 3
+
+# mid_index = len(fruits) // 2
+
+
+# print(fruits[mid_index:])
+# print(fruits[:mid_index])
+
+# import random
+
+# total = 0
+# counter = 0
+
+# while counter < 100:
+#     random_number = random.randint(1,20)
+#     total += random_number
+#     counter+=1
+
+# print(counter)
+
+# food_item = []
+# while True:
+#     food = input("What would you like to have?")
+#     if(food == "end"):
+#         break
+#     food_item.append(food)
+
+# for food in food_item:
+#     print(food)
