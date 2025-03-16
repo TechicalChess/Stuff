e66b1328a97f94b18985e9eef61598b6f80550be
TechicalChess <chua_yun_jie@students.edu.sg>
2025-03-16 08:06:46 +0000

    Mar 16, 2025, 8:06 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -43,3 +43,52 @@
 
 # for food in food_item:
 #     print(food)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# List
+import random
+counter = []
+Health = 100
+counter = str
+print("Hero starts on his adventure with Health: 100")
+# Random health lost
+while counter < 100:
+     random_number = random.randint(1,15)
+     Health -= random_number
+     counter+=1
+
+# End
+if Health < 1:
+    print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died."):
+else:
+    print("After fighting monsters, his Health is now:" + Health)
